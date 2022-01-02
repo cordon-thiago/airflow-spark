ee0175faf0f4a4d5e4de2e1c22700ddcc231fb8d
cvmartin <cvarelamail@gmail.com>
2022-01-02 20:25:39 +0100

    better dag to populate knmi weather

---
+++ code/dags/populate_knmi_weather_hour.py
@@ -0,0 +1,98 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.dummy import DummyOperator
+import psycopg2
+
+import requests
+import zipfile
+import os
+import shutil
+
+import pandas as pd
+import datetime
+import sys
+
+sys.path.append("/usr/local/modules")
+from parsers.knmi_weather_hour import parse, push
+
+
+def download_uurgegevens_txt (file_name, destination_folder):
+    """
+    Download uurgegevens data from KNMI
+    """
+    url = f"https://cdn.knmi.nl/knmi/map/page/klimatologie/gegevens/uurgegevens/{file_name}"
+    r = requests.get(url, allow_redirects=True)
+    
+    open(f"{destination_folder}/{file_name}", 'wb').write(r.content)
+    
+    with zipfile.ZipFile(f"{destination_folder}/{file_name}", 'r') as zip_ref:
+        zip_ref.extractall(f"{destination_folder}")
+        
+    return True
+
+def download_data():
+    # just files from de_bilt station (for showcase)
+    files_to_download = ["uurgeg_260_2011-2020.zip", "uurgeg_260_2021-2030.zip"]
+
+    cache_folder = "/tmp/de_bilt_cache"
+    
+    # create cache folder
+    if os.path.exists(cache_folder) is False:
+        os.mkdir(cache_folder)
+    
+    for file in files_to_download:
+      download_uurgegevens_txt(file, cache_folder)
+      print(f"downloaded {file} in {cache_folder}")
+      
+    return True
+  
+def parse_and_upload():
+  
+  cache_folder = "/tmp/de_bilt_cache"
+  
+  files_to_upload = [f"{cache_folder}/{x}" for x in os.listdir(cache_folder) if x.endswith(".txt")]
+  
+  conn = psycopg2.connect(
+        host = "postgres",
+        database = "test",
+        user = "test",
+        password = "postgres"
+      )
+  
+  for file in files_to_upload:
+      push(conn=conn, df=parse(file))
+      
+  conn.close()
+      
+  return True
+  
+# DAG
+dag = DAG(
+    dag_id="populate_knmi_weather_hour",
+    start_date=datetime.datetime(2020, 2, 2),
+    schedule_interval="@once",
+    catchup=False,
+    tags=['setup']
+)
+
+start_populate = DummyOperator(
+        task_id='start_populate',
+        dag=dag)
+
+download_data = PythonOperator(
+    task_id="download_data",
+    python_callable=download_data,
+    dag=dag
+)
+
+parse_and_upload_data = PythonOperator(
+    task_id="parse_and_upload_data",
+    python_callable=parse_and_upload,
+    dag=dag
+)
+
+end_populate = DummyOperator(
+    task_id='end_populate',
+    dag=dag)
+      
+start_populate >> download_data >> parse_and_upload_data >> end_populate
---
+++ code/modules/parsers/knmi_weather_hour.py
@@ -0,0 +1,50 @@
+import sys
+
+sys.path.append("/usr/local/modules")
+from database_interaction import postgres_insert_df
+
+import pandas as pd
+
+def parse(file):
+    df = pd.read_csv(
+        file,
+        sep=",",
+        index_col=False,
+        skiprows=_search_string_by_line(file, "# STN,YYYYMMDD")[0] - 1,
+        skipinitialspace=True
+    )
+
+    station_code = df["# STN"][0]
+
+    df['datetime'] = pd.to_datetime(df['YYYYMMDD'], format='%Y%m%d', utc=True)
+    df['datetime'] = df['datetime'] + pd.to_timedelta(df['HH'] - 1, 'hours')
+
+    df = df.rename(columns={"T": "temperature", "Q": "solar_radiation"})
+    df['temperature'] = df['temperature'] / 10
+    # pass from (J/h)/cm2 to W/m2
+    df["solar_radiation"] = df["solar_radiation"] * ((100 * 100) / (60 * 60))
+    df['station_id'] = station_code
+
+    return df[['station_id', 'datetime', 'temperature', 'solar_radiation']]
+
+
+def push(conn, df):
+    postgres_insert_df(conn=conn, df=df, table="knmi_weather_hour")
+    print("dataframe pushed to knmi_weather_hour")
+    return True
+
+def _search_string_by_line(file_name, string_to_search):
+    """Search for the given string in file and return line numbers with
+    that string"""
+    line_number = 0
+    list_of_results = []
+    # Open the file in read only mode
+    with open(file_name, 'r') as read_obj:
+        # Read all lines in the file one by one
+        for line in read_obj:
+            # For each line, check if line contains the string
+            line_number += 1
+            if string_to_search in line:
+                # If yes, then add the line number & line as a tuple in the list
+                list_of_results.append(line_number)
+    return list_of_results
