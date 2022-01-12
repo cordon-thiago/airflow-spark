d90affbe9e2114769ceae2237f407edc08ef3193
cvmartin <cvarelamail@gmail.com>
2022-01-12 18:16:27 +0100

    better queries

--- code/sql/select_template/whale_backcast_per_model.sql
+++ code/sql/select_template/whale_backcast_per_model.sql
@@ -1,3 +1,6 @@
-select *
+select id, model_id, datetime_insert, date_backcast_from, date_backcast_until,
+cast(metrics ->> 'rmse' as float) as RMSE,
+cast(metrics ->> 'mae' as float) as MAE,
+cast(metrics ->> 'mape' as float) as MAPE
 from whale_backcast wb
-where model_id = {{model_id}}
+where wb.model_id = {{model_id}}
